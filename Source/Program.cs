5b912dc66f48dfbc9f4800e9b91842a2a1bff98d
Quinton <86534924+QuinnCG@users.noreply.github.com>
2024-04-03 19:29:32 -0400

    Added console app options to select which app to launch while in release mode

--- Source/Program.cs
+++ Source/Program.cs
@@ -1,4 +1,5 @@
 ﻿using OpenGLSandbox.Applications;
+using System.Text;
 
 namespace OpenGLSandbox;
 
@@ -6,7 +7,42 @@ static class Program
 {
 	private static void Main()
 	{
+#if !DEBUG
+		var builder = new StringBuilder();
+		builder.AppendLine("Applications:");
+
+		var appTypes = new Type[]
+		{
+			typeof(TriangleApp),
+			typeof(QuadApp)
+		};
+
+		for (int i = 0; i < appTypes.Length; i++)
+		{
+			builder.AppendLine($"\t({i + 1}) {appTypes[i].Name}");
+		}
+
+		Console.WriteLine(builder);
+		Console.WriteLine("Select application by number:");
+
+		while (true)
+		{
+			var key = Console.ReadKey(true).KeyChar;
+			int index = key - '1';
+
+			if (index < appTypes.Length)
+			{
+				Console.Clear();
+
+				var app = Activator.CreateInstance(appTypes[index]) as Application;
+				app!.Run();
+
+				break;
+			}
+		}
+#else
 		var app = new QuadApp();
 		app.Run();
+#endif
 	}
 }
